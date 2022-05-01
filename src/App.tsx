4d504044797492c74f8a29fb89bdbf23d3f068df
Uisma Lopes <uismalopes@gmail.com>
2022-05-01 16:23:24 -0300

    create template

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "@styles/global.scss";
+import Home from "@pages/Home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HelloWorld from "@components/HelloWorld";
+
+function Home() {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default Home;
